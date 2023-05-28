451d7e5fb4ac3d8fb086f01cbd993e878b87057a
dem0-0 <dem0-0@example.com>
2023-05-29 02:01:30 +0300

    class based websocket bugfix

--- data_collector/controllers/websocket_events.py
+++ data_collector/controllers/websocket_events.py
@@ -2,10 +2,12 @@ from .base_controller import BaseController
 
 from flask_socketio import Namespace
 
-class WebsocketEvents(Namespace, BaseController):
+class WebsocketEvents(Namespace):
     def __init__(self, namespace: str, base_container):
-        super(Namespace, self).__init__(namespace)
-        super(BaseController, self).__init__(base_container)
+        super().__init__(namespace)
+        
+        self.config_service = base_container.config_service
+        self.data_service = base_container.data_service
         
     def on_connect(self):
         pass
